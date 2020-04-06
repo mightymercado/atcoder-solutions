be17cc3c689744693308fb09e494d2c9cd36b448
Ken Mercado <mightymercado@gmail.com>
2020-04-06 20:16:13 +0800

    C - Vacation

---
+++ Educational DP Round/C.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <array>
+#include <cmath>
+using i64 = long long;
+using namespace std;
+
+const int maxn = 1e5;
+i64 a[maxn + 1][3] = {0}, dp[maxn][3] = {0};
+
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++) 
+    for (int j = 0; j < 3; j++)
+      cin >> a[i][j];
+  for (int i = 1; i <= n; i++)
+    for (int j = 0; j < 3; j++)
+      for (int k = 0; k < 3; k++)
+        if (j != k)
+          dp[i][j] = max(dp[i][j], dp[i - 1][k] + a[i][k]);
+  cout << max(dp[n][0], dp[n][1]) << endl;
+}
